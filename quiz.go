4db5450e505c208a10e44432173852466e19a80e
Devon <devonlumsden@mail.com>
2022-03-20 17:18:45 +1300

    load csv file

--- quiz.go
+++ quiz.go
@@ -1,11 +1,33 @@
 package main
 
 import "fmt"
+import "encoding/csv"
+import "os"
+import "log"
 
 func display_question(){
 	fmt.Println("Here is a question for you")
 }
 
+func load_questions(){
+	f, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+//	questionsList := createQuestionsList(data)
+
+	fmt.Printf("%+v\n", data)
+}
+
 func main() {
-	display_question()
+	//display_question()
+	load_questions()
 }
